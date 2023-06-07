7a6fcd126a19be2da7249d759400602f5a33ff4a
Fabien <fabien.ors@mines-paristech.fr>
2023-06-07 22:57:13 +0200

    Fix wxGLCanvas

--- main.cpp
+++ main.cpp
@@ -14,6 +14,75 @@
                     //Not necessary, but if it was, it needs to be replaced by process.h AND io.h
 #endif
 
+
+#ifdef WIN32
+// The following function is taken nearly verbatim from
+// http://www.halcyon.com/~ast/dload/guicon.htm
+void RedirectIOToConsole()
+{
+  int hConHandle;
+  long lStdHandle;
+  FILE *fp;
+
+  // redirect unbuffered STDOUT to the console
+  lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+  hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+  fp = _fdopen( hConHandle, "w" );
+  *stdout = *fp;
+  setvbuf( stdout, NULL, _IONBF, 0 );
+
+  // redirect unbuffered STDIN to the console
+  lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+  hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+  fp = _fdopen( hConHandle, "r" );
+  *stdin = *fp;
+  setvbuf( stdin, NULL, _IONBF, 0 );
+
+  // redirect unbuffered STDERR to the console
+  lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+  hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+  fp = _fdopen( hConHandle, "w" );
+  *stderr = *fp;
+  setvbuf( stderr, NULL, _IONBF, 0 );
+
+  // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
+  // point to console as well
+  std::ios::sync_with_stdio();
+}
+
+class UseConsole
+{
+public:
+  UseConsole();
+  ~UseConsole();
+  static void AttachToConsole();
+  static void DetachFromConsole();
+private:
+  static bool _good;
+};
+
+bool UseConsole::_good = false;
+UseConsole::UseConsole() {
+}
+
+UseConsole::~UseConsole() {
+}
+
+void UseConsole::AttachToConsole() {
+  if (!_good) {
+    _good = AttachConsole(ATTACH_PARENT_PROCESS);
+    if (_good)
+      RedirectIOToConsole();
+  }
+}
+
+void UseConsole::DetachFromConsole() {
+  if (_good)
+    FreeConsole();
+}
+
+#endif /* WIN32 */
+
 class wxGLCanvasSubClass: public wxGLCanvas {
         void Render();
 public:
@@ -21,16 +90,25 @@ public:
     void Paintit(wxPaintEvent& event);
 protected:
     DECLARE_EVENT_TABLE()
+private:
+    // OpenGL context (to be initialized when the window is shown)
+    wxGLContext *_glContext;
 };
 
 BEGIN_EVENT_TABLE(wxGLCanvasSubClass, wxGLCanvas)
     EVT_PAINT    (wxGLCanvasSubClass::Paintit)
 END_EVENT_TABLE()
 
+// Double buffering (Fix B70)
+static int wx_gl_attribs[] = {WX_GL_RGBA, WX_GL_DOUBLEBUFFER, WX_GL_DEPTH_SIZE, 24, 0};
+
 wxGLCanvasSubClass::wxGLCanvasSubClass(wxFrame *parent)
-:wxGLCanvas(parent, wxID_ANY,  wxDefaultPosition, wxDefaultSize, 0, wxT("GLCanvas")){
+: wxGLCanvas(parent, wxID_ANY, wx_gl_attribs, wxDefaultPosition, wxDefaultSize, 0, wxT("GLCanvas"))
+{
     int argc = 1;
     char* argv[1] = { wxString((wxTheApp->argv)[0]).char_str() };
+    // OpenGL context initialization
+    _glContext = new wxGLContext(this);
 
 /*
 NOTE: this example uses GLUT in order to have a free teapot model
@@ -48,7 +126,7 @@ void wxGLCanvasSubClass::Paintit(wxPaintEvent& WXUNUSED(event)){
 
 void wxGLCanvasSubClass::Render()
 {
-    SetCurrent();
+    SetCurrent(*_glContext);
     wxPaintDC(this);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -89,17 +167,32 @@ class MyApp: public wxApp
 {
     virtual bool OnInit();
     wxGLCanvas * MyGLCanvas;
+#ifdef WIN32
+    virtual int OnExit() ;
+#endif
 };
 
  
-IMPLEMENT_APP(MyApp)
+IMPLEMENT_APP_CONSOLE(MyApp) ;
  
   
 bool MyApp::OnInit()
 {
+#ifdef WIN32
+    UseConsole::AttachToConsole();
+    std::cout << std::endl;
+#endif
+
     wxFrame *frame = new wxFrame((wxFrame *)NULL, -1,  wxT("Hello GL World"), wxPoint(50,50), wxSize(200,200));
-    new wxGLCanvasSubClass(frame);
+    wxGLCanvasSubClass* canvas = new wxGLCanvasSubClass(frame);
  
     frame->Show(TRUE);
     return TRUE;
 }
+
+#ifdef WIN32
+int MyApp::OnExit() {
+  UseConsole::DetachFromConsole();
+  return wxApp::OnExit();
+}
+#endif
